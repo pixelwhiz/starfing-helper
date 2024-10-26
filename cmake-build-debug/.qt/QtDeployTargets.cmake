9c776dd3d91dc9950efab966ed8a3ba280cdcf99
pixelwhiz <daffa01futn@gmail.com>
2024-10-26 16:10:14 +0700

    initial commit

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_strafe-helper_FILE D:/daffa/dev/@tools/strafe-helper/cmake-build-debug/strafe-helper.exe)
-set(__QT_DEPLOY_TARGET_strafe-helper_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_strafe-helper_RUNTIME_DLLS D:/daffa/game/Qt/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/daffa/game/Qt/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/daffa/game/Qt/6.8.0/mingw_64/bin/Qt6Core.dll)
