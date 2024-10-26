937116206d5175a3faa5e14550b4d8ba395d3e32
pixelwhiz <daffa01futn@gmail.com>
2024-10-26 15:45:54 +0700

    initial commit

--- main.cpp
+++ main.cpp
@@ -1,24 +1,69 @@
 #include "app.h"
 #include <QApplication>
+#include <QPushButton>
+#include <windows.h>
+#include <thread>
+#include <atomic>
 
 void initWindow(app &w);
+void program(std::atomic<bool> &run);
+
+std::atomic<bool> runFlag(false);
+std::thread runThread;
 
 int main(int argc, char *argv[])
-{
+{00000
     QApplication a(argc, argv);
     app w;
 
     initWindow(w);
 
+    QPushButton* startButton = w.findChild<QPushButton*>("pushButton");
+    if (startButton) {
+        QObject::connect(startButton, &QPushButton::clicked, [&]() {
+            if (!runFlag) {
+                runFlag = true;
+                runThread = std::thread(program, std::ref(runFlag));
+                startButton->setText("Stop");
+            } else {
+                runFlag = false;
+                if (runThread.joinable()) {
+                    runThread.join();
+                }
+                startButton->setText("Start");
+            }
+        });
+    }
+
     w.show();
-    return a.exec();
+
+    int result = a.exec();
+
+    runFlag = false;
+    if (runThread.joinable()) {
+        runThread.join();
+    }
+
+    return result;
 }
 
 void initWindow(app &w) {
     w.setFixedSize(500, 300);
     w.setWindowIcon(QIcon(":/assets/icon.png"));
     w.setWindowTitle("Strafe Helper");
-
     w.setWindowFlags(w.windowFlags() & ~Qt::WindowMaximizeButtonHint);
 }
 
+void program(std::atomic<bool> &run) {
+    while (run) {
+        if (GetAsyncKeyState(VK_LBUTTON) & 0x8000) {
+            keybd_event(0x30, 0, 0, 0);
+        } else {
+            keybd_event(0x30, 0, KEYEVENTF_KEYUP, 0);
+        }
+
+        Sleep(10);
+    }
+
+    keybd_event(0x30, 0, KEYEVENTF_KEYUP, 0);
+}
